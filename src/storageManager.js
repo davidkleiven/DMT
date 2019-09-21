1b693a48fa6012a7ea4e1ecb59b8140738e2181c
David Kleiven <davidkleiven446@gmail.com>
2019-09-21 16:03:24 +0200

    Add storage manager

---
+++ src/storageManager.js
@@ -0,0 +1,35 @@
+import { AsyncStorage } from "react-native";
+
+class StorageManager{
+    _retrieveData = async (key) => {
+        try{
+            var value = await AsyncStorage.getItem(key);
+            return value;
+        } catch(error){
+            console.log("Could not retrieve data!");
+            console.log(error);
+        }
+        return null;
+    }
+
+    _storeData = async (key, value) => {
+        try{
+            await AsyncStorage.setItem(key, JSON.stringify(value));
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    updateCommand = (cmd) => {
+        this._retrieveData(cmd).then((numCalls) => {
+            var num = 1;
+            if (numCalls !== null){
+                num = parseInt(numCalls);
+                num += 1;
+            }
+            this._storeData(cmd, num);
+        });
+    }
+}
+
+export default StorageManager;
